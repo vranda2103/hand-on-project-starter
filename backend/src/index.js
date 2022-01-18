3c2a383e90f44e4dcc39c52c25033de13cea24d5
Vranda <vrandaagarwal15092001@gmail.com>
2022-01-18 13:43:25 +0530

    database intitialization and setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("Connected to MongoDB database");
+})
+.catch((err) => {
+  console.log("Error connecting to database "+err);
+});
 app.get("/helloWorld", (req, res) => {
   res.send("Hello World");
 });
